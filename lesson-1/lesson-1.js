df29b34cb7f6697feabfe4de6574bfd469c357e5
apolonskyi <apolonskyi@alteryx.com>
2022-06-13 15:47:21 +0200

    init

---
+++ lesson-1/lesson-1.js
@@ -0,0 +1,27 @@
+// Exercise. 
+// Need to change the order of result variable "a" and "b" using only one additional variable
+let a = 'green';
+let b = 'red';
+
+// Your code
+
+
+console.log(a); // Should equal 'red'
+console.log(b); // Should equal 'green'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
