9cebd0ca931e2e4345fdf3e77d1132b8d729bcd0
apolonskyi <apolonskyi@alteryx.com>
2022-07-04 21:03:04 +0200

    lesson 7

---
+++ lesson-7/modules/new-import-export/app.js
@@ -0,0 +1,10 @@
+// import { add, subtract } from './data';
+// import add from './data';
+import myDefaultFunc, * as supeHeroes from './client.js';
+
+// console.log(ABC);
+// console.log(add(1, 2));
+// console.log(`The addition is: ${add(2, 3)}`);
+supeHeroes.marvel('Venom');
+supeHeroes.dc('Wondar Woman');
+console.log(myDefaultFunc());
---
+++ lesson-7/modules/new-import-export/client.js
@@ -0,0 +1,17 @@
+export const marvel = (hero) => {
+    console.log(`Hello, ${hero}!`);
+  }
+
+export class ABC {
+  
+}
+  
+export const dc = (hero) => {
+  console.log(`Bye, ${hero}!`);
+}
+
+const defaultFunc = () => 'Im Default';
+
+export default defaultFunc;
+
+// export {marvel, dc};
